9fd20783e3ba6685e01b112c3f2d0e8a09fa4b12
ALoca19 <anitaynala17@gmail.com>
2025-06-12 21:26:12 -0600

    valor de z0 y0 x5

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ class App {
 			this.isManualControlEnabled = true;
 			if (this.controls.isLocked) this.controls.unlock();
 			this.sceneGroup.position.y = 0; // Subir la escena en VR (ajusta este valor si es necesario)
-			this.sceneGroup.position.x = 1.5; 
+			this.sceneGroup.position.x = 5; 
 			this.sceneGroup.position.z = 0; 
 		});
 
@@ -240,7 +240,7 @@ class App {
 
             const targetPosition = new THREE.Vector3(
                 //planetInfo.position.x + (planetInfo.name === 'Sol' ? 2 : posicionXPlus),
-				planetInfo.position.x+1.5,
+				planetInfo.position.x,
                 planetInfo.position.y,
                 planetInfo.position.z 
             );

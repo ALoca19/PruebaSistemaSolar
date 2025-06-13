5e31e0ff61efc80a0be843e9a9bc2dd641029ed5
ALoca19 <anitaynala17@gmail.com>
2025-06-12 20:22:16 -0600

    valor de z en +4

--- app.js
+++ app.js
@@ -172,9 +172,9 @@ class App {
 			console.log('Sesión VR iniciada');
 			this.isManualControlEnabled = true;
 			if (this.controls.isLocked) this.controls.unlock();
-			this.sceneGroup.position.y = -2; // Subir la escena en VR (ajusta este valor si es necesario)
+			this.sceneGroup.position.y = 0; // Subir la escena en VR (ajusta este valor si es necesario)
 			this.sceneGroup.position.x = 0; 
-			this.sceneGroup.position.z = 0; 
+			this.sceneGroup.position.z = 4; 
 		});
 
         this.renderer.xr.addEventListener('sessionend', () => {
@@ -241,8 +241,8 @@ class App {
             const targetPosition = new THREE.Vector3(
                 //planetInfo.position.x + (planetInfo.name === 'Sol' ? 2 : posicionXPlus),
 				planetInfo.position.x,
-                planetInfo.position.y-2,
-                planetInfo.position.z
+                planetInfo.position.y,
+                planetInfo.position.z+4
             );
 
             const sceneGroupTarget = this.renderer.xr.isPresenting

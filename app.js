a7573646d8b11dc2dada52c5636fea05079b9374
ALoca19 <anitaynala17@gmail.com>
2025-06-13 10:17:57 -0600

    valor de z3.5 y1.5 x0 del modelo, agregamos distancia de camara en X y z1.5

--- app.js
+++ app.js
@@ -241,8 +241,8 @@ class App {
             const targetPosition = new THREE.Vector3(
                 //planetInfo.position.x + (planetInfo.name === 'Sol' ? 2 : posicionXPlus),
 				planetInfo.position.x + (planetInfo.name === 'Sol' ? 2 : posicionXPlus),
-                planetInfo.position.y + 1.5,
-                planetInfo.position.z 
+                planetInfo.position.y,
+                planetInfo.position.z + 1.5 
             );
 
             const sceneGroupTarget = this.renderer.xr.isPresenting
